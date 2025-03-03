d0bea868c910047236a7ff49f248cad914d85985
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2025-03-03 16:19:31 +1100

    Revert "Merge pull request #6 from Plattko/feature-Damned-Soul"
    This reverts commit 7c132edc6131bb2416641c6b1108a1fc2f83c12e, reversing
    changes made to d0cb5373a572aa4b484ecacc30ad18901b9890de.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,6 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody rb;
 
     private Vector3 moveInput;
-    private bool canMove = true;
     [SerializeField] private float moveSpeed = 3.0f;
     [SerializeField] private float idleSlow = 0.9f;
 
@@ -23,55 +22,24 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float dashCooldown = 1.0f;
 
     [Header("Sprite")]
-    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] SpriteRenderer spriteRenderer;
 
-    [Header("Spellcasting")]
-    [SerializeField] private SpellManager spellManager;
-
-    [Header("Interacting")]
-    [SerializeField] private Interactor interactor;
-
-    private void OnEnable()
+    void Start()
     {
-        if (spellManager != null)
-        {
-            spellManager.onSpellCast += OnSpellCast;
-        }
-        else
-        {
-            Debug.LogWarning("No Spell Manager detected.");
-        }
+        
     }
 
-    private void OnDisable()
+    
+    void Update()
     {
-        if (spellManager != null)
-        {
-            spellManager.onSpellCast -= OnSpellCast;
-        }
-        else
-        {
-            Debug.LogWarning("No Spell Manager detected.");
-        }
+        //if (isDashing) { return; }
     }
 
     private void FixedUpdate()
     {
-        // Do nothing if the player can't move or is dashing
-        if (!canMove || isDashing) { return; }
-        // Update the player's movement
+        if (isDashing) { return; }
         Move();
-        // Update the player's facing direction
         Flip();
-    }
-
-    private void OnSpellCast(float castTime, bool lockoutDuringCast)
-    {
-        // Disable movement for the duration of the cast if the spell has a lockout during its cast
-        if (lockoutDuringCast)
-        {
-            StartCoroutine(MovementLockout(castTime));
-        }
     }
 
     //-------------------------------------------------------------
@@ -79,57 +47,37 @@ public class PlayerController : MonoBehaviour
     //-------------------------------------------------------------
     private void Move()
     {
-        // If there is move input, set the player's velocity to the move speed in the direction of the input
         if (moveInput != Vector3.zero)
         {
             rb.velocity = moveInput * moveSpeed;
         }
-        // If there is no move input, lerp the player's velocity to 0 using the idle slow speed
         else
         {
             rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, idleSlow);
         }
     }
 
-    private IEnumerator MovementLockout(float duration)
-    {
-        // Disable movement
-        canMove = false;
-        // Set the player's velocity to 0
-        rb.velocity = Vector3.zero;
-        // Wait for the lockout duration
-        yield return new WaitForSeconds(duration);
-        // Re-enable movement
-        canMove = true;
-    }
-
     private IEnumerator Dash() // TODO: Make player unable to be damaged when dashing
     {
-        // Disable the ability to dash
         canDash = false;
-        // Set dashing to true
         isDashing = true;
-        // Set the player's velocity to speed required to travel the dash's distance over its duration in the direction of the move input
         rb.velocity = moveInput * (dashDistance / dashDuration);
-        // Wait for the dash duration and set dashing to false
         yield return new WaitForSeconds(dashDuration);
         isDashing = false;
-        // Wait for the dash cooldown duration and re-enable the ability to dash
         yield return new WaitForSeconds(dashCooldown);
         canDash = true;
     }
 
-    //-------------------------------------------------------------
+    // ---------------------------------
     // SPRITE & ANIMATIONS
-    //-------------------------------------------------------------
+    // ---------------------------------
     private void Flip()
     {
-        // If the player changes direction, update their facing direction
         if (isFacingRight && moveInput.x < 0 || !isFacingRight && moveInput.x > 0)
         {
             isFacingRight = !isFacingRight;
         }
-        // Flip the player's sprite in the direction they are facing
+
         spriteRenderer.flipX = isFacingRight;
     }
 
@@ -138,33 +86,14 @@ public class PlayerController : MonoBehaviour
     //-------------------------------------------------------------
     public void OnMove(InputAction.CallbackContext context)
     {
-        // Set the move input on the x and z axis
         moveInput = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
     }
 
     public void OnDash(InputAction.CallbackContext context)
     {
-        // If the input is pressed and the player can dash, dash
         if (context.performed && canDash)
         {
             StartCoroutine(Dash());
-        }
-    }
-
-    public void OnUseSpell(InputAction.CallbackContext context)
-    {
-        // If the input is pressed, cast a spell through the Spell Manager script
-        if (context.performed)
-        {
-            spellManager.CastSpell();
-        }
-    }
-
-    public void OnInteract(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            interactor.Interact();
         }
     }
 }
--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -4,69 +4,19 @@ using UnityEngine.UI;
 public class PlayerUI : MonoBehaviour
 {
     public PlayerCharacter playerCharacter;
-    [SerializeField] private GameObject player;
-    private SpellManager spellManager;
-
-    [Header("Health")]
     public Image[] heartImages;  // Array of Image components to show hearts
 
-    [Header("Spells")]
-    [SerializeField] private Image spellIcon;
-    [SerializeField] private Transform manaIcons;
-
-    private void Awake()
+    void Start()
     {
-        spellManager = player.GetComponentInChildren<SpellManager>();
-    }
-
-    private void OnEnable()
-    {
-        if (spellManager != null)
-        {
-            spellManager.onManaUpdated += UpdateManaUI;
-            spellManager.onSpellUpdated += UpdateSpellUI;
-        }
-        else
-        {
-            Debug.LogWarning("No Spell Manager detected.");
-        }
-    }
-
-    private void OnDisable()
-    {
-        if (spellManager != null)
-        {
-            spellManager.onManaUpdated -= UpdateManaUI;
-            spellManager.onSpellUpdated -= UpdateSpellUI;
-        }
-        else
-        {
-            Debug.LogWarning("No Spell Manager detected.");
-        }
-    }
-
-    private void Start()
-    {
-        //Debug.Log("this script is working");
+        Debug.Log("this script is working");
         playerCharacter.InitializeHealth();
         UpdateHeartUI();
-    }
-
-    void Update()
-    {
-        //// Simulating damage with space bar for testing
-        //if (Input.GetKeyDown(KeyCode.E))
-        //{
-        //    Debug.Log("Damage is being done");
-        //    playerCharacter.TakeDamage(1);
-        //    UpdateHeartUI();
-        //}
     }
 
     // Call this whenever the player's health changes
     public void UpdateHeartUI()
     {
-        //Debug.Log("hearts updating");
+        Debug.Log("hearts updating");
         Sprite[] hearts = playerCharacter.GetHearts();
 
         for (int i = 0; i < heartImages.Length; i++)
@@ -75,32 +25,14 @@ public class PlayerUI : MonoBehaviour
         }
     }
 
-    private void UpdateManaUI(int curMana)
+    void Update()
     {
-        Debug.Log("Updated mana: " +  curMana);
-
-        // Show a number of mana icons equal to the player's current mana
-        for (int i = 0; i < manaIcons.childCount; i++)
+        // Simulating damage with space bar for testing
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            if (i < curMana)
-            {
-                manaIcons.GetChild(i).gameObject.SetActive(true);
-            }
-            else
-            {
-                manaIcons.GetChild(i).gameObject.SetActive(false);
-            }
+            Debug.Log("Damage is being done");
+            playerCharacter.TakeDamage(1);
+            UpdateHeartUI();
         }
-    }
-
-    private void UpdateSpellUI(Spell newSpell)
-    {
-        // Show the spell icon if it is hidden
-        if (!spellIcon.enabled)
-        {
-            spellIcon.enabled = true;
-        }
-        // Set the spell icon to the new spell's icon
-        spellIcon.sprite = newSpell.icon;
     }
 }
