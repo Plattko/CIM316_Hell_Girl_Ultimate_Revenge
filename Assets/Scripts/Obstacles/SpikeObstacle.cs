016d9022086c3be48435658a74c35d0797e1ed2a
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2025-04-10 19:06:42 +1000

    Added Spike and Fire Obstacles
    - Imported fire and flipped fire obstacle sprite sheets.
    - Created Test_Obstacles scene for testing the new obstacles.
    - Created new Test_ObstacleRoom prefab for building and testing the new obstacles.
    - Created SpikeObstacle script that manages the spike trap being triggered by the player/enemies.
    - Created FireObstacle script that manages the fire obstacle being destroyed by player attacks.
    - Created ObstacleHitbox script that deals different damage amounts when it collides with the player or enemies.
    - Created SpikeObstacle1x1, SpikeObstacle2x2 and FireObstacle animation controllers.
    - Created idle and triggered animations for both spike obstacles.
    - Created idle animation for fire obstacle.
    - Created SpikeObstacle1x1, SpikeObstacle2x2 and FireObstacle prefabs with animations and functionality.

---
+++ Assets/Scripts/Obstacles/SpikeObstacle.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeObstacle : MonoBehaviour
+{
+    private Animator anim;
+
+    private bool canTrigger = true;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Trigger(other);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Trigger(other);
+    }
+
+    private void Trigger(Collider other)
+    {
+        if (!canTrigger) return;
+
+        if (other.CompareTag("Player") || other.CompareTag("Enemy"))
+        {
+            canTrigger = false;
+
+            anim.SetTrigger("trigger");
+        }
+    }
+
+    public void EnableTrigger()
+    {
+        canTrigger = true;
+    }
+}
