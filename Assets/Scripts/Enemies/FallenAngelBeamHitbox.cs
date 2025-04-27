5248b95be7cc297793741383d68bb32947cd18e1
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2025-04-28 02:58:00 +1000

    Fixed Fallen Angel Damaging Itself with Beam
    - Created FallenAngelBeamHitbox script that works the same as the ObstacleHitbox script but ignores the object if it has the FallenAngel script.
    - Replaced ObstacleHitbox script with FallenAngelBeamHitbox in FallenAngelBeam prefab.

---
+++ Assets/Scripts/Enemies/FallenAngelBeamHitbox.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallenAngelBeamHitbox : MonoBehaviour
+{
+    [SerializeField] private int playerDamage = 1;
+    [SerializeField] private float enemyDamage = 10f;
+    [SerializeField] private bool dealKnockback;
+    [SerializeField] private float knockbackStrength = 5f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DealDamage(other, playerDamage);
+        }
+        else if (other.CompareTag("Enemy"))
+        {
+            // If the object has the Fallen Angel script, do nothing
+            if (other.GetComponentInChildren<FallenAngel>() != null) return;
+            
+            DealDamage(other, enemyDamage);
+        }
+    }
+
+    private void DealDamage(Collider other, float damage)
+    {
+        // Deal damage if it hit a damageable object
+        IDamageable damageable = other.GetComponent<IDamageable>();
+        if (damageable != null)
+        {
+            // Deal damage
+            damageable.TakeDamage(damage);
+        }
+        // Apply knockback if object is knockbackable
+        if (dealKnockback && other.TryGetComponent(out IKnockbackable knockbackable))
+        {
+            Vector3 direction = (other.transform.position - transform.position).normalized;
+            direction.y = 0;
+            knockbackable.Knockback(direction, knockbackStrength);
+        }
+    }
+}
